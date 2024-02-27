a4e6455f0edecb866f94045d3ec7c18b4352d3d9
Nvcq <nicolas.mopin@gmail.com>
2024-02-27 12:27:04 +0100

    setup bdd

--- src/views/MissionSearch.vue
+++ src/views/MissionSearch.vue
@@ -1,9 +1,27 @@
 <template>
-    <div>
-        <h1>Mission Search page</h1>
-    </div>
+  <div>
+    <h1>Mission Search page</h1>
+    <ul>
+      <li v-for="npc in npcs" :key="npc.npc_id">{{ npc.name }}</li>
+      <li>Test</li>
+    </ul>
+  </div>
 </template>
+
 <script setup>
+import { ref, onMounted } from "vue";
+import { supabase } from "../lib/supabaseClient";
+
+const npcs = ref([]);
+
+async function getNpcs() {
+  const { data } = await supabase.from("npc").select();
+  npcs.value = data;
+}
+
+onMounted(() => {
+  getNpcs();
+});
 </script>
-<style scoped>
-</style>
+
+<style scoped></style>
