b4ece898ba29e002fab36c62e0e2298f22ef20f7
Wintxer <47366828+Wintxer@users.noreply.github.com>
2024-02-27 14:46:49 +0100

    JDRI-24 buttoncomponent + html homepage

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <button id='button' class="mt-4">
+        <p> {{title}}</p>
+    </button>
+</template>
+    
+<script>
+export default {
+    props: ['title']
+}
+</script>
+
+<style scoped>
+#button p{
+    background: #0A98FF;
+    text-decoration: none;
+    color: #fff;
+    padding: 7px 50px;
+}
+</style>
