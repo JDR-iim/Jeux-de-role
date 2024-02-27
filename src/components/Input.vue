65b5f492623de9634b488fa61ee01b1a82ad0b54
fideline <fideline.bazana-ntomo@charlie-solutions.com>
2024-02-27 17:08:43 +0100

    login form

---
+++ src/components/Input.vue
@@ -0,0 +1,33 @@
+<!-- ChildComponent.vue -->
+<template>
+    <div>
+    <label :for="inputId">{{ labelText }}</label>
+    <input :type="inputType" :id="inputId" v-model="value" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+    inputType: {
+        type: String,
+        default: 'text' // Définir le type par défaut comme 'text'
+    },
+    labelText: {
+        type: String,
+        default: 'Label' // Définir le label par défaut
+    }
+    },
+    data() {
+    return {
+        value: ''
+    };
+    },
+    computed: {
+    inputId() {
+        // Générer un ID unique pour l'input basé sur le texte du label
+        return `input-${this.labelText.toLowerCase().replace(/\s/g, '-')}`;
+    }
+    }
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,36 +1,19 @@
 <template>
-  <div>
-    <h1>Login page</h1>
-    <br />
-    <input placeholder="Email" v-model="email" type="email" /> <br />
-    <br />
-    <input placeholder="Password" v-model="password" type="password" /> <br />
-    <br />
-    <button @click="signIn()">Sign in</button>
-  </div>
+    <div class="flex">
+        <div>
+            <h1>Login page</h1>
+            <div>
+                <Input inputType="text" labelText="Nom"  />
+            </div>
+        </div>
+        <div>
+            hkjl
+        </div>
+    </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { supabase } from "../lib/supabaseClient";
-import { useRouter } from "vue-router";
-
-const email = ref("");
-const password = ref("");
-const router = useRouter();
-
-async function signIn() {
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email: email.value,
-    password: password.value,
-  });
-
-  if (error) {
-    alert("Error logging in : " + error.message);
-  } else {
-    router.push("/");
-  }
-}
+    import Input from '../components/Input.vue'
 </script>
 
 <style scoped></style>
