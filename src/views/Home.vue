ec92b1a681d2ef64c42cb35aecfbc86fd065c5e0
Nvcq <nicolas.mopin@gmail.com>
2024-02-27 16:13:50 +0100

    added signup, login, logout and routes protection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-        <h1>Home page</h1>
-    </div>
+  <div>
+    <h1>Home page</h1>
+  </div>
 </template>
-<script setup>
-</script>
-<style scoped>
-</style>
+
+<script setup></script>
+
+<style scoped></style>
--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,9 +1,42 @@
 <template>
-    <div>
-        <h1>User profile page</h1>
-    </div>
+  <div>
+    <h1>User profile page</h1>
+    <br />
+    <p v-if="account">
+      Hello
+      {{
+        account.data.session.user.user_metadata.username
+          ? account.data.session.user.user_metadata.username
+          : account.data.session.user.email
+      }}
+    </p>
+    <br />
+    <button @click="logout()">Logout</button>
+  </div>
 </template>
+
 <script setup>
+import { ref } from "vue";
+import { supabase } from "../lib/supabaseClient";
+import { useRouter } from "vue-router";
+
+const account = ref();
+const router = useRouter();
+getSession();
+
+async function getSession() {
+  account.value = await supabase.auth.getSession();
+}
+
+async function logout() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    alert("Error logging out: " + error.message);
+  } else {
+    router.push("/login");
+  }
+}
 </script>
-<style scoped>
-</style>
+
+<style scoped></style>
