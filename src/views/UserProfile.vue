4b4b28d3c01d5668d1b2e53d946a5f1aa7640bd9
coralieO <coralie.ouedraogo@edu.devinci.fr>
2024-02-28 09:50:11 +0100

    fetch person

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -10,10 +10,13 @@
           : account.data.session.user.email
       }}
     </p>
+    <li v-for="character in characters" :key="character.id">{{ character.name }}</li>
+
     <br />
     <button @click="logout()">Logout</button>
   </div>
 </template>
+
 
 <script setup>
 import { ref } from "vue";
@@ -23,6 +26,18 @@ import { useRouter } from "vue-router";
 const account = ref();
 const router = useRouter();
 getSession();
+
+const characters = ref([]);
+
+async function getcharter() {
+  const { data } = await supabase.from("Characters").select();
+  console.log(data)
+  characters.value = data;
+}
+
+onMounted(() => {
+  getcharter();
+});
 
 async function getSession() {
   account.value = await supabase.auth.getSession();
